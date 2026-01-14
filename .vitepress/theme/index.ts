4d8c16a0745434f7d191711ef10325cfb14da5b3
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2026-01-14 20:43:58 +0400

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,10 +2,14 @@ import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
+import ArsCards from '../components/ArsCards.vue'
+
 export default {
   extends: DefaultTheme,
   Layout() {
-    return h(DefaultTheme.Layout, null, {
-    })
+    return h(DefaultTheme.Layout, null, {})
+  },
+  enhanceApp({ app }) {
+    app.component('ArsCards', ArsCards)
   }
 }
